d9a5d9eb96d6118d0bfb6e62f89c2155dd58495a
Ирина Патрикеева <patrikeevairina25@router.lab209.kaf>
2021-10-04 20:44:25 +0300

    init commit

--- lab_1.0/main.c
+++ lab_1.0/main.c
@@ -12,9 +12,7 @@ void exit_call()
 int main()
 {
 	pid_t pid;
-	//pid = fork();
 	atexit(exit_call);
-	//pid = fork();
 	switch ( pid = fork() )
 	{
 		case 0:
@@ -30,7 +28,7 @@ int main()
 			printf("\nIt's parent\n");
 			printf("Pid: %d\n", getpid());
                         printf("Ppid %d\n", getppid());
-			printf("Waiting\n");
+			printf("\nWaiting for the child process to terminate\n");
 			waitpid(pid, &st, 0);
 			printf("Return status of the child process: %d\n", WEXITSTATUS(st));
 			
