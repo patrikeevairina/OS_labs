75dfef2ccce14fd863fcf25c79d4768320504549
Ирина Патрикеева <patrikeevairina25@router.lab209.kaf>
2021-11-03 00:41:36 +0300

    segfault

--- lab_0.7/main.c
+++ lab_0.7/main.c
@@ -3,27 +3,15 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 
-typedef struct 
+typedef struct
 {
 	char filename[256];
-	long filesize;
+	long fileSize;
 	unsigned int mode;
 	char *fileData;
 } structArchive;
 
-char *getFileName(char *filePath) 
-{
-	for (unsigned long i = strlen(filePath) - 1; i; i--) 
-	{
-		if (filePath[i] == '/') 
-		{
-			return &filePath[i + 1];
-		}
-	}
-	return filePath;
-}
-
-long getFileSize(char *filePath) 
+long getFileSize(char *filePath)
 {
 	FILE *fp1 = fopen(filePath, "rb");
 	long prev = ftell(fp1);
@@ -34,46 +22,76 @@ long getFileSize(char *filePath)
 	return sz;
 }
 
-void addToArchive(structArchive f, char *path) 
+void addToArchive(structArchive f, char *archName)
 {
-	FILE *fp = fopen(path, "ab");
-	if (fwrite(&f.filesize, sizeof(f.filesize), 1, fp)) 
+	FILE *fp = fopen(archName, "ab");
+	if (fwrite(&f.fileSize, sizeof(f.fileSize), 1, fp)) 
 	{
 		fwrite(&f.filename, sizeof(char) * 256 + 1, 1, fp);
 		fwrite(&f.mode, sizeof(unsigned int), 1, fp);
-		fwrite(f.fileData, f.filesize, 1, fp);
+		fwrite(f.fileData, f.fileSize, 1, fp);
 	}
 	fclose(fp);
 }
 
-int main(int argc, char *argv[]) 
+void deleteFromArchive(char *archName, char *extractingFile)
 {
-	char *arg1 = argv[1];
-	char *arg2 = argv[2];
-	char *arg3 = argv[3];
+	//FILE *in = fopen("buf", "wr");
+	FILE *out = fopen(archName, "rb");
+//	printf("almost ok");
+        structArchive f;
+	while (fread(&f.fileSize, sizeof(long) + 1, 1, out))
+	{
+		fread(&f.filename, sizeof(char) * 256 + 1, 1, out);
+		char* filename = f.filename;
+		fread(&f.mode, sizeof(unsigned int), 1, out);
+		f.fileData = (char *) malloc(sizeof(char) * (f.fileSize + 1));
+		fread(f.fileData, f.fileSize, 1, out);
+		if (strcmp(filename, extractingFile) != 0) 
+		{
+	//		printf("ok");
+			addToArchive(f, "buf");
+		}
+	}
+	//printf("ok");
+	fclose(out);
+	//fclose(in);
+	/*FILE *arch  = fopen(archName, "wb");
+	FILE *bufArch = fopen("buf", "rb");
+	int ch;
+	ch = fgetc(bufArch);
+	while(!feof(bufArch))
+	{
+		fputc(ch, arch);
+		ch = fgetc(bufArch);
+	}
+	fclose(bufArch);
+	fclose(arch);*/
+}
 
-	if (strcmp(arg1, "-h") == 0 || strcmp(arg1, "--help") == 0) 
+int main(int argc, char **argv)
+{
+	if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) 
 	{
 		printf("./archiver arch_name –i(--input) file1\n"
 			   "./archiver arch_name –e(--extract) file1\n"
 			   "./archiver arch_name –s(--stat) // вывести текущее состояние архива \n"
 			   "./archiver –h(--help) // вывести справку по работе с архиватором \n");
-	} 
-	else if (strcmp(arg2, "-i") == 0 || strcmp(arg2, "--input") == 0) 
+	}
+	else if (strcmp(argv[2], "-i") == 0 || strcmp(argv[2], "--input") == 0) 
 	{
 		structArchive archive;
-		char *f = getFileName(arg3);
 		struct stat stats;
-		strcpy(archive.filename, f);
-		archive.filesize = getFileSize(arg3);
+		strcpy(archive.filename, argv[3]);
+		archive.fileSize = getFileSize(argv[3]);
 		stat(archive.filename, &stats);
 		archive.mode = stats.st_mode;
-		archive.fileData = (char *) malloc(sizeof(char) * (archive.filesize + 1));
+		archive.fileData = (char *) malloc(sizeof(char) * (archive.fileSize + 1));
 
-		FILE *fp = fopen(arg3, "rb");
+		FILE *fp = fopen(argv[3], "rb");
 		int i = 0;
 		char ch;
-		long size = getFileSize(arg3);
+		long size = getFileSize(argv[3]);
 		char *fileData = (char *) malloc(sizeof(char) * size + 1);
 		while ((ch = fgetc(fp)) != EOF) 
 		{
@@ -81,52 +99,35 @@ int main(int argc, char *argv[])
 			i++;
 		}
 		fclose(fp);
-
 		strcpy(archive.fileData, fileData);
-		addToArchive(archive, arg1);
-		if (remove(arg3) != 0)
+		addToArchive(archive, argv[1]);
+		if (remove(argv[3]) != 0)
 			return -1;
 	}
-	else if (strcmp(arg2, "-s") == 0 || strcmp(arg2, "--stat") == 0) 
+	else if (strcmp(argv[2], "-e") == 0 || strcmp(argv[2], "--extract") == 0)
 	{
-		FILE *fp = fopen(arg1, "rb");
+		FILE *fp = fopen(argv[1], "rb");
 		structArchive f;
-		while (fread(&f.filesize, sizeof(long), 1, fp)) 
-		{
-			fread(&f.filename, sizeof(char) * 256 + 1, 1, fp);
-			fread(&f.mode, sizeof(unsigned int), 1, fp);
-			f.fileData = (char *) malloc(sizeof(char) * (f.filesize + 1));
-			fread(f.fileData, f.filesize, 1, fp);
-			printf("%s\t%ld\t%u\n", f.filename, f.filesize, f.mode);
-		}
-		fclose(fp);
-	}
-	
-	else if (strcmp(arg2, "-e") == 0 || strcmp(arg2, "--extract") == 0) 
-	{
-		
-		FILE *fp = fopen(arg1, "rb");
-		structArchive f;
-
-		while (fread(&f.filesize, sizeof(long), 1, fp)) 
+		while (fread(&f.fileSize, sizeof(long), 1, fp)) 
 		{
 			fread(&f.filename, sizeof(char) * 256 + 1, 1, fp);
 			char *filename = f.filename;
 			fread(&f.mode, sizeof(unsigned int), 1, fp);
-			f.fileData = (char *) malloc(sizeof(char) * (f.filesize + 1));
-			fread(f.fileData, f.filesize, 1, fp);
+			f.fileData = (char *) malloc(sizeof(char) * (f.fileSize + 1));
+			fread(f.fileData, f.fileSize, 1, fp);
 
-			if (strcmp(filename, arg3) == 0 || strcmp(arg3, "all") == 0) 
+			if (strcmp(filename, argv[3]) == 0 || strcmp(argv[3], "all") == 0) 
 			{
 				FILE *fp = fopen(filename, "wb");
 				char *fileData = f.fileData;
 				fputs(fileData, fp);
 				chmod(filename, f.mode);
 				printf("%s extracted %u\n", filename, f.mode);
-				if (strcmp(arg3, "all") != 0) break;
+				if (strcmp(argv[3], "all") != 0) break;
 			}
 		}
 		fclose(fp);
+		deleteFromArchive(argv[1], argv[3]);
 	}
 	return 0;
 }
